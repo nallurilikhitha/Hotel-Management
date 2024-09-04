390296599805781a75ff7761a98972bdf2deddc9
nallurilikhitha <165470158+nallurilikhitha@users.noreply.github.com>
2024-09-04 12:23:30 +0530

    Update UpdateCheck.java

--- Hotel-Management/Hotel-Management-System-master/Hotel Management System/src/hotel/management/system/UpdateCheck.java
+++ Hotel-Management/Hotel-Management-System-master/Hotel Management System/src/hotel/management/system/UpdateCheck.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hotel.management.system;
 
 import java.awt.BorderLayout;
@@ -47,10 +43,7 @@ public class UpdateCheck extends JFrame {
 		this.dispose();
 	}
 
-	/**
-	 * Create the frame.
-	 * @throws SQLException 
-	 */
+	
 	public UpdateCheck() throws SQLException {
 		//conn = Javaconnect.getDBConnection();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
