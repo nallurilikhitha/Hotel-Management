5d8079c7092edb23a9b25128c4cfa92db7db608e
nallurilikhitha <165470158+nallurilikhitha@users.noreply.github.com>
2024-09-04 12:09:38 +0530

    Update AddDrivers.java

--- Hotel-Management/Hotel-Management-System-master/Hotel Management System/src/hotel/management/system/AddDrivers.java
+++ Hotel-Management/Hotel-Management-System-master/Hotel Management System/src/hotel/management/system/AddDrivers.java
@@ -29,11 +29,11 @@ public class AddDrivers extends JFrame implements ActionListener{
 	contentPane.setLayout(null);
         
         ImageIcon i1  = new ImageIcon(ClassLoader.getSystemResource("hotel/management/system/icons/eleven.jpg"));
-                Image i3 = i1.getImage().getScaledInstance(500, 300,Image.SCALE_DEFAULT);
-                ImageIcon i2 = new ImageIcon(i3);
-                JLabel l15 = new JLabel(i2);
-                l15.setBounds(400,30,500,370);
-                add(l15);
+        Image i3 = i1.getImage().getScaledInstance(500, 300,Image.SCALE_DEFAULT);
+        ImageIcon i2 = new ImageIcon(i3);
+	JLabel l15 = new JLabel(i2);
+        l15.setBounds(400,30,500,370);
+        add(l15);
         
         JLabel l10 = new JLabel("Add Drivers");
         l10.setFont(new Font("Tahoma", Font.BOLD, 18));
