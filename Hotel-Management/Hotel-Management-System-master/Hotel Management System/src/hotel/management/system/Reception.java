0cb459dfdadfdad26be6ff4aa8c9076a72366602
nallurilikhitha <165470158+nallurilikhitha@users.noreply.github.com>
2024-09-04 12:21:34 +0530

    Update Reception.java

--- Hotel-Management/Hotel-Management-System-master/Hotel Management System/src/hotel/management/system/Reception.java
+++ Hotel-Management/Hotel-Management-System-master/Hotel Management System/src/hotel/management/system/Reception.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hotel.management.system;
 
 import javax.swing.*;
@@ -91,7 +87,7 @@ public class Reception extends JFrame {
 
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("All Employee Info");
+		JButton btnNewButton_2 = new JButton(" Employee Info");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try{
